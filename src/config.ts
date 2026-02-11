b0ef7243f62464900a34f4701799d147a7fefc93
KRLY05 <krly05@gmail.com>
2026-02-11 16:36:22 +0100

    Initial setup

---
+++ src/config.ts
@@ -0,0 +1,23 @@
+export const SITE = {
+  website: "https://publisher.pages.dev/", // replace this with your deployed domain
+  author: "KRLY05",
+  profile: "https://satnaing.dev/",
+  desc: "Auto translated articles",
+  title: "KRLY05",
+  ogImage: "publisher-og.jpg",
+  lightAndDarkMode: true,
+  postPerIndex: 4,
+  postPerPage: 4,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  showArchives: true,
+  showBackButton: true, // show back button in post detail
+  editPost: {
+    enabled: true,
+    text: "Edit page",
+    url: "https://github.com/KRLY05/publisher/edit/main/",
+  },
+  dynamicOgImage: true,
+  dir: "ltr", // "rtl" | "auto"
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+} as const;
---
+++ src/pages/about.md
@@ -0,0 +1,8 @@
+---
+layout: ../layouts/AboutLayout.astro
+title: "About"
+---
+
+Publisher is a webapp with collection of auto-translated articles, built with [Astro](https://astro.build/), [Tailwind CSS](https://tailwindcss.com/) and based on [astro-paper](https://github.com/satnaing/astro-paper).
+
+![Astro Paper](public/publisher-og.jpg)
